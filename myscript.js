305cb1a720fe9afe3fa35d30d2223068d336a4ba
adrdeatcu <150624496+adrdeatcu@users.noreply.github.com>
2025-04-27 22:52:52 +0300

    feat: making the changing size feature of the grid

--- myscript.js
+++ myscript.js
@@ -1,4 +1,7 @@
 let container =  document.getElementById("container");
+let popupButton = document.getElementById("popupButton")
+
+popupButton.addEventListener("click", enterGridSize);
 
 for(let i=1;i<=256;i++)
 {
@@ -10,4 +13,33 @@ for(let i=1;i<=256;i++)
     container.appendChild(div);
 }
 
+function gridCreation(value)
+{
+
+    container.innerHTML="";
+    for(let i=1;i<=value*value;i++)
+    {
+        let div=document.createElement("div");
+        div.classList.add("square");
+        div.style.flex=`0 0 ${100/value}%`;
+        div.addEventListener("mouseenter", function(){
+            div.style.backgroundColor = "black";
+        })
+        container.appendChild(div);
+    }
+    
+
+}
+
+function enterGridSize()
+{
+    let popupMessage= Number(prompt("Enter the grid size(max 64)"));
+    if(popupMessage>=65)
+    {
+        popupMessage=Number(prompt("Enter the grid size(max 64)"));
+    }
+    gridCreation(popupMessage);
+    
+    
+}
 
